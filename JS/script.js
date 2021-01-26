e16b100a738fd8dee1650eb92780ef413d38f2ab
maxemga <maxemga22@gmail.com>
2021-01-26 15:57:06 +0300

    add overlay

--- JS/script.js
+++ JS/script.js
@@ -3,14 +3,39 @@ let inputResult = document.getElementById("input2").value;
 
 let result = document.getElementById("result");
 
-function GetButton(num) {
+let previewText = document.getElementById("text");
+let overlayLeft = document.getElementById("OverlayLeft");
+let overlayRight = document.getElementById("OverlayRight");
 
-        document.getElementById("input1").value  =  
-        document.getElementById("input1").value + num;
+// window.onload = function() {
+//     previewText.classList.toggle("previewTextActive");
+// }
 
-   
+window.addEventListener("load", ShowText);
 
+previewText.addEventListener("click", CloseText)
     
+
+
+function ShowText() {
+    previewText.classList.toggle("previewTextActive");  
+}
+function CloseText() {
+    previewText.classList.remove("previewTextActive");
+    setTimeout(CloseOverlay, 800);
+}
+function CloseOverlay() {
+    overlayLeft.classList.add("blockOverlayLeftActive");
+    overlayRight.classList.add("blockOverlayRightActive");
+}
+
+
+
+
+// Calculator
+function GetButton(num) {
+    document.getElementById("input1").value  =  
+    document.getElementById("input1").value + num;
 }
 
 function ClearText() {
